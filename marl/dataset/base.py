8ee77fc1467a75de095b45d94d8bc44588bc424d
lishuaibin <lishuaibin@pjlab.org.cn>
2024-02-26 08:36:29 +0000

    feat: add txt data loader

---
+++ marl/dataset/base.py
@@ -0,0 +1,103 @@
+""" Basic datasets implement. """
+
+import gzip
+import json
+import random
+from contextlib import contextmanager
+from torch.utils.data import IterableDataset
+
+
+@contextmanager
+def open_file(filename):
+    """Construct a file handler.
+
+    The handler can read a normal file or a file compressed by `gzip`.
+    """
+    if filename.endswith(".gz"):
+        fp = gzip.open(filename, "rt")
+    else:
+        fp = open(filename, "r", encoding="utf-8")
+    yield fp
+    fp.close()
+
+
+class InfiniteDataset(IterableDataset):
+    """ Load infinite data from original dataset with shuffle. """
+
+    def __init__(self, dataset, rng=None):
+        self.data = list(iter(dataset))
+        self.indices = list(range(len(self.data)))
+        if rng is None:
+            rng = random.Random()
+        self.rng = rng
+
+    def __iter__(self):
+        while True:
+            self.rng.shuffle(self.indices)
+            for i in self.indices:
+                yield self.data[i]
+
+
+class FileDataset(IterableDataset):
+    """Single file dataset."""
+
+    def __init__(self, filename):
+        self._filename = filename
+
+    def __iter__(self):
+        with open_file(self._filename) as fin:
+            for lineno, line in enumerate(fin):
+                ex = json.loads(line)
+                if ex is None:
+                    continue
+                yield ex
+
+
+class MultiSourceDatset(IterableDataset):
+    """Multiple source dataset."""
+
+    def __init__(self, task_group_filename, sub_dataset_type="file", random_seed=1024):
+        with open(task_group_filename) as fin:
+            tasks = json.load(fin)
+            # filter zero probability task
+            tasks = [task for task in tasks if task["prob"] > 0]
+            self._task_group = tasks
+        if sub_dataset_type == "file":
+            for task in self._task_group:
+                task["dataset"] = FileDataset(task["filepath"])
+        else:
+            raise NotImplementedError("Cannot support filelist now.")
+        sum_prob = sum([task["prob"] for task in self._task_group])
+        for task in self._task_group:
+            task["prob"] = task["prob"] / sum_prob
+
+        self.random_seed = random_seed
+
+    def __iter__(self):
+        rng = random.Random(self.random_seed)
+        probs = [task["prob"] for task in self._task_group]
+        # Initialize task iterator
+        for task in self._task_group:
+            task["iterator"] = iter(task["dataset"])
+        while True:
+            task = rng.choices(self._task_group, weights=probs)[0]
+            try:
+                yield from task["iterator"]
+            except StopIteration:
+                task["iterator"] = iter(task["dataset"])
+                yield from task["iterator"]
+
+
+if __name__ == "__main__":
+    dataset_config = {
+        "task_group_filename": "marl/config/task_sft.json",
+        "max_seq_len": 4096,
+        "num_samples_each_epoch": 8,
+        "random_seed": 1
+    }
+
+    example_dataset = MultiSourceDatset(
+        task_group_filename=dataset_config["task_group_filename"],
+        sub_dataset_type="file",
+    )
+    print(example_dataset)
---
+++ marl/dataset/txt_loader.py
@@ -0,0 +1,115 @@
+""" Finetuning dataset. """
+import random
+from typing import List
+import numpy as np
+import logging
+from dataclasses import dataclass
+from torch.utils.data import IterableDataset
+from base import MultiSourceDatset, InfiniteDataset
+
+
+@dataclass
+class Sequence:
+    token_ids: List[int]
+    group: str = None
+    idx: str = None
+
+
+class TxtSequenceDataset(IterableDataset):
+    """ Create sequences from dataset.
+    """
+    def __init__(self,
+                 dataset: MultiSourceDatset,
+                 tokenizer=None,
+                 start_token = "[UNUSED_TOKEN_146]user\n",
+                 end_token = "[UNUSED_TOKEN_145]\n",
+                 max_seq_len: int = 4096,
+                 num_samples_each_epoch: int = 64,
+                 is_valid: bool = False,
+                 random_seed: int = 110,
+                 **kwargs
+                 ):
+        self.example_dataset = dataset
+        self.tokenizer = tokenizer #TODO
+        self.start_token = start_token
+        self.end_token = end_token
+        self.max_seq_len = max_seq_len
+        self.num_samples_each_epoch = num_samples_each_epoch
+        self.is_valid = is_valid
+        
+        self.random_seed = random_seed
+        self.rng = random.Random(self.random_seed)
+        np.random.seed(self.random_seed)
+        random.seed(self.random_seed)
+        if not is_valid:
+            samples_cnts = []
+            for task in self.example_dataset._task_group:
+                task["target_num_each_epoch"] = int(task["prob"] * num_samples_each_epoch + 0.5)
+                inner_dataset = InfiniteDataset(task["dataset"], self.rng)
+                task["iterator"] = iter(inner_dataset)
+                samples_cnts.append(task["target_num_each_epoch"])
+                print(f"{task['filepath']}: task prob: {task['prob']}, "
+                        f"ori number of examples: {len(inner_dataset.data)}, "
+                        f"target_num_each_epoch: {task['target_num_each_epoch']}")
+            assert sum(samples_cnts) == num_samples_each_epoch, "[Dataset init] sample num error"
+            print(f"[Txt] Training dataset initialized, random seed {self.random_seed}.")
+            print()
+        self.epoch_index = 0
+
+    def __iter__(self):
+        while True:
+            # epoch_rng only use in this epoch.
+            epoch_rng = np.random.RandomState(self.epoch_index)
+            # prepare epoch data
+            # print(f"prepare TxtSequenceDataset for epoch {self.epoch_index}...")
+            examples_all = []
+            for task in self.example_dataset._task_group:
+                if self.is_valid:
+                    examples = [ex for ex in task["dataset"]]
+                else:
+                    examples = [next(task["iterator"]) for _ in range(task["target_num_each_epoch"])]
+                print(f"prepare {len(examples)} data from {task['filepath']}")
+                epoch_rng.shuffle(examples)
+                examples_all.extend(examples)
+            epoch_rng.shuffle(examples_all)
+            print(f"prepare TxtSequenceDataset done: total number of examples is {len(examples_all)} for epoch {self.epoch_index}.")
+
+            batch_sequence = []
+            for index, example in enumerate(examples_all):
+                sequence = self._postprocess_sequence(example)
+                batch_sequence.append(sequence)
+            assert len(batch_sequence) == self.num_samples_each_epoch, f"[Epoch {self.epoch_index}] Wrong data len"
+            yield batch_sequence
+
+            self.epoch_index += 1
+
+    def _postprocess_sequence(self, example):
+        """Post process sequence: tokenization & truncation."""
+        # print(example)
+        question, answer = example["question"].strip(), example.get("answer", "").strip()
+        
+        # tokens_question, tokens_answer = self.tokenizer.tokenize(question), self.tokenizer.tokenize(answer)
+        tokens_question, tokens_answer = question, answer
+        
+        tokens = (tokens_question
+                    + tokens_answer
+                    )
+
+        if len(tokens) <= 4:
+            return None
+        
+        # tokens = [self.start_token] + tokens
+        tokens = self.start_token + tokens
+
+        assert len(tokens) <= self.max_seq_len, "{}-{}".format(len(tokens), self.max_seq_len)
+
+        if len(tokens) > self.max_seq_len:
+            raise RuntimeError(f"token_ids is too long: {len(tokens)}")
+        # token_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+        token_ids = tokens
+        pos_ids = list(range(len(token_ids)))
+        
+        return Sequence(token_ids=token_ids,
+                        group=example["group"],
+                        idx=example["id"]
+                        )

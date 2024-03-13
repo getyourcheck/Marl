ff3dc1d5ff2995a9254b91f51a34432f85a3f300
zhaohui <zhaohui@pjlab.org.cn>
2024-03-13 03:21:29 +0000

    fix: minor on infer test

--- tests/models/test_models_infer.py
+++ tests/models/test_models_infer.py
@@ -91,15 +91,13 @@ def test_generate():
         "eos_token_id": 92542,
         "pad_token_id": 0,
     }
-    meta_instruction = """
-        You are an AI assistant whose name is InternLM (书生·浦语).
-        - InternLM (书生·浦语) is a conversational language model that is developed by Shanghai AI Laboratory (上海人工智能实验室). It is designed to be helpful, honest, and harmless.
-        - InternLM (书生·浦语) can understand and communicate fluently in the language chosen by the user such as English and 中文
-        """.replace(" ", "")
+    meta_instruction = """You are an AI assistant whose name is InternLM (书生·浦语).
+- InternLM (书生·浦语) is a conversational language model that is developed by Shanghai AI Laboratory (上海人工智能实验室). It is designed to be helpful, honest, and harmless.
+- InternLM (书生·浦语) can understand and communicate fluently in the language chosen by the user such as English and 中文"""
 
     chat_template = ""
     chat_template += "{{ bos_token }}"
-    chat_template += f"{{'<|im_start|>system\n{meta_instruction}<|im_end|>\n'}}"
+    chat_template += "{{'<|im_start|>system\n" + meta_instruction + "<|im_end|>\n'}}"
     chat_template += "{% for message in messages %}"
     chat_template += "{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}"
     chat_template += "{% endfor %}"
@@ -117,14 +115,14 @@ def test_generate():
     question_mask = policy_output.question_mask
     answer_mask = policy_output.answer_mask
     expected = [
-        "非常好，有什么需要我帮助的吗？",
-        """好的，以下是三个关于管理时间的建议：
+        "你好，有什么可以帮助你的吗？",
+        """好的，以下是三条关于管理时间的建议：
 
-1. 制定计划和时间表： 计划是管理时间的关键。制定一个合理的计划和时间表可以帮助您更好地组织自己的时间，并确保您有足够的时间来完成任务。在计划和时间表上，您应该包括每天、每周、每月的任务和目标。确保您的计划和时间表清晰、具体、可行，并根据需要进行调整。
+1. 制定计划：每天开始之前，列出您需要完成的任务和目标，并根据重要性和优先级安排优先级。根据截止日期的截止时间来安排时间表，并遵循计划。
 
-2. 优先级管理： 将任务按照优先级排序，以确保您首先完成最重要的任务。将时间分配给每个任务，并尽力保证每个任务都有一个足够的时间。如果有多个任务需要完成，请确保优先处理最紧急和最重要的任务，以确保您的生产力最大化。
+2. 确定优先事项：将任务分为紧急和重要、紧急且重要、重要但不紧急、紧急但不重要、既不紧急也不重要的五个类别。然后按照重要性对任务进行排序，优先完成重要且紧急或重要但不紧急的任务。
 
-3. 学会说“不”： 有时候，我们可能被要求做很多不必要的事情或任务，这可能会浪费我们的时间。学会说“不”可以避免这种情况发生。当您接受不必要的任务时，请确保您有足够的时间来完成其他重要的任务，并为它们腾出时间。""",
+3. 管理时间：不要在一项任务上花费过多时间，避免浪费太多时间在低优先级任务上。尝试避免多任务处理，而是专注于一项任务直到完成。另外，利用工具和技术，如番茄钟、待办事项应用程序等，可以帮助您更好地管理时间并提高生产力。""",
     ]
     input_strs = [
         tokenizer.apply_chat_template(
@@ -136,54 +134,56 @@ def test_generate():
         for input in input_strs
     ]
     input_ids = tokenizer(input_strs, return_tensors="pt", padding=True).input_ids
-
+    question_len = input_ids.shape[-1]
+    answer_1_len = tokenizer([expected[0]], return_tensors="pt", padding=True).input_ids.shape[-1]
+    output_len = output_ids.shape[-1]
     assert (
-        tokenizer.decode(output_ids[0][input_ids.shape[-1] : 134]) == expected[0]
-    ), f"expected: {expected[0]!r}\noutput: {tokenizer.decode(output_ids[0][input_ids.shape[-1]:135])!r}"
+        tokenizer.decode(output_ids[0][question_len : question_len + answer_1_len - 1]) == expected[0]
+    ), f"expected: {expected[0]}\noutput: {tokenizer.decode(output_ids[0][question_len : question_len + answer_1_len])}"
     assert (
-        tokenizer.decode(output_ids[1][input_ids.shape[-1] : -1]) == expected[1]
-    ), f"expected: {expected[1]!r}\noutput: {tokenizer.decode(output_ids[1][input_ids.shape[-1]:-1])!r}"
+        tokenizer.decode(output_ids[1][question_len : -1]) == expected[1]
+    ), f"expected: {expected[1]}\noutput: {tokenizer.decode(output_ids[1][question_len : -1])}"
 
     # check question_mask
     assert torch.equal(
         question_mask[0][:6], torch.zeros(6, dtype=int)
     ), f"expected: {torch.zeros(6,dtype=int)}\noutput: {question_mask[0][:6]}"  # padding
     assert torch.equal(
-        question_mask[0][6 : input_ids.shape[-1]],
-        torch.ones(input_ids.shape[-1] - 6, dtype=int),
-    ), f"expected: {torch.ones(input_ids.shape[-1] - 6,dtype=int)}\noutput: {question_mask[0][6:input_ids.shape[-1]]}"  # question
+        question_mask[0][6 : question_len],
+        torch.ones(question_len - 6, dtype=int),
+    ), f"expected: {torch.ones(question_len - 6, dtype=int)}\noutput: {question_mask[0][6 : question_len]}"  # question
     assert torch.equal(
-        question_mask[0][input_ids.shape[-1] :],
-        torch.zeros(question_mask.shape[-1] - input_ids.shape[-1], dtype=int),
-    ), f"expected: {torch.zeros(question_mask.shape[-1] - input_ids.shape[-1],dtype=int)}\noutput: {question_mask[0][input_ids.shape[-1]:]}"  # answer
+        question_mask[0][question_len :],
+        torch.zeros(output_len - question_len, dtype=int),
+    ), f"expected: {torch.zeros(output_len - question_len, dtype=int)}\noutput: {question_mask[0][question_len :]}"  # answer
 
     assert torch.equal(
-        question_mask[1][: input_ids.shape[-1]],
-        torch.ones(input_ids.shape[-1], dtype=int),
-    ), f"expected: {torch.ones(input_ids.shape[-1],dtype=int)}\noutput: {question_mask[1][:input_ids.shape[-1]]}"  # question
+        question_mask[1][: question_len],
+        torch.ones(question_len, dtype=int),
+    ), f"expected: {torch.ones(question_len, dtype=int)}\noutput: {question_mask[1][: question_len]}"  # question
     assert torch.equal(
-        question_mask[1][input_ids.shape[-1] :],
-        torch.zeros(question_mask.shape[-1] - input_ids.shape[-1], dtype=int),
-    ), f"expected: {torch.zeros(question_mask.shape[-1] - input_ids.shape[-1],dtype=int)}\noutput: {question_mask[1][input_ids.shape[-1]:]}"  # answer
+        question_mask[1][question_len :],
+        torch.zeros(output_len - question_len, dtype=int),
+    ), f"expected: {torch.zeros(output_len - question_len, dtype=int)}\noutput: {question_mask[1][question_len :]}"  # answer
 
     # check answer_mask
     assert torch.equal(
-        answer_mask[0][: input_ids.shape[-1]],
-        torch.zeros(input_ids.shape[-1], dtype=int),
-    ), f"expected: {torch.zeros(input_ids.shape[-1],dtype=int)}\noutput: {answer_mask[0][:input_ids.shape[-1]]}"  # question
+        answer_mask[0][: question_len],
+        torch.zeros(question_len, dtype=int),
+    ), f"expected: {torch.zeros(question_len, dtype=int)}\noutput: {answer_mask[0][: question_len]}"  # question
     assert torch.equal(
-        answer_mask[0][input_ids.shape[-1] : 135],
-        torch.ones(135 - input_ids.shape[-1], dtype=int),
-    ), f"expected: {torch.ones(135 - input_ids.shape[-1],dtype=int)}\noutput: {answer_mask[0][input_ids.shape[-1]:135]}"  # answer
+        answer_mask[0][question_len : question_len + answer_1_len],
+        torch.ones(answer_1_len, dtype=int),
+    ), f"expected: {torch.ones(answer_1_len, dtype=int)}\noutput: {answer_mask[0][question_len : question_len + answer_1_len]}"  # answer
     assert torch.equal(
-        answer_mask[0][135:], torch.zeros(answer_mask.shape[-1] - 135, dtype=int)
-    ), f"expected: {torch.zeros(answer_mask.shape[-1] - 135,dtype=int)}\noutput: {answer_mask[0][135:]}"  # padding
+        answer_mask[0][question_len + answer_1_len:], torch.zeros(output_len - question_len - answer_1_len, dtype=int)
+    ), f"expected: { torch.zeros(output_len - question_len - answer_1_len, dtype=int)}\noutput: {answer_mask[0][question_len + answer_1_len:]}"  # padding
 
     assert torch.equal(
-        answer_mask[1][: input_ids.shape[-1]],
-        torch.zeros(input_ids.shape[-1], dtype=int),
-    ), f"expected: {torch.zeros(input_ids.shape[-1],dtype=int)}\noutput: {answer_mask[1][:input_ids.shape[-1]]}"  # question
+        answer_mask[1][: question_len],
+        torch.zeros(question_len, dtype=int),
+    ), f"expected: {torch.zeros(question_len, dtype=int)}\noutput: {answer_mask[1][: question_len]}"  # question
     assert torch.equal(
-        answer_mask[1][input_ids.shape[-1] :],
-        torch.ones(answer_mask.shape[-1] - input_ids.shape[-1], dtype=int),
-    ), f"expected: {torch.ones(answer_mask.shape[-1] - input_ids.shape[-1],dtype=int)}\noutput: {answer_mask[1][input_ids.shape[-1]:]}"  # answer
+        answer_mask[1][question_len :],
+        torch.ones(output_len - question_len, dtype=int),
+    ), f"expected: {torch.ones(output_len - question_len, dtype=int)}\noutput: {answer_mask[1][question_len :]}"  # answer
